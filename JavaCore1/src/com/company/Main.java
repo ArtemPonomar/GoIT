c76bd7f396d2f6c89bbb5184a14dd061e5fd124f
ArtemPonomar <37250267+ArtemPonomar@users.noreply.github.com>
2018-08-05 21:00:54 +0300

    JavaCore1_1
    Homework1

---
+++ JavaCore1/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t\tHow i spent my summer holidays\n\tMade by Ponomarenko Artem\n\tFirst month was spent on taking university exams.\nThen, most of the time i spent at home, sometimes going out cycling with my friends\nAlso i completely ruined my sleep schedule, going to bad at 5 a.m. and waking up at 2 p.m. ");
+    }
+}
