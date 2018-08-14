3ca0c9117e2ff676804c52d2150c3a0ae42a9e3d
ArtemPonomar <37250267+ArtemPonomar@users.noreply.github.com>
2018-08-14 13:02:01 +0300

    Homework2_1
    Implemented user dialog

--- Homework2/src/main.java
+++ Homework2/src/main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.nextLine();
+        System.out.print("Enter your сity of residence: ");
+        String city = in.nextLine();
+        System.out.print("Enter your age: ");
+        String age = in.nextLine();
+        System.out.print("Enter your hobby: ");
+        String hobby = in.nextLine();
     }
 }
