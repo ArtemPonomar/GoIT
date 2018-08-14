a7e1c84c0b7279e4ec747c18f9c48b22c86ebf70
ArtemPonomar <37250267+ArtemPonomar@users.noreply.github.com>
2018-08-14 13:21:30 +0300

    Homework2_2
    implemented output

--- Homework2/src/main.java
+++ Homework2/src/main.java
@@ -2,14 +2,38 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
+
         System.out.print("Enter your name: ");
         String name = in.nextLine();
+
         System.out.print("Enter your сity of residence: ");
         String city = in.nextLine();
+
         System.out.print("Enter your age: ");
         String age = in.nextLine();
+
         System.out.print("Enter your hobby: ");
         String hobby = in.nextLine();
+
+        System.out.println("-------------------------");
+        System.out.println("Name:\t" + name);
+        System.out.println("City:\t" + city);
+        System.out.println("Age:\t" + age);
+        System.out.println("Hobby:\t" + hobby);
+
+        System.out.println("-------------------------");
+        System.out.println("Human named " + name + " lives in " + city + ".");
+        System.out.println("He's " + age + " years old and loves " + hobby);
+
+        System.out.println("-------------------------");
+        System.out.println(name + "\t - Name");
+        System.out.println(city + "\t - City");
+        System.out.println(age + "\t\t - Age");
+        System.out.println(hobby + "\t - Hobby");
+
+        System.out.println("-------------------------");
+
     }
 }
